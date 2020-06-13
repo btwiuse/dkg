347312f03302ca5cebb1e1011ec79f87c58fda4c
zouhangbiao <zouhangbiao@powerlaw.ai>
2020-06-13 23:37:12 +0800

    bump to github.com/google/go-containerregistry v0.1.1

--- pkg/imgpkg/image/registry.go
+++ pkg/imgpkg/image/registry.go
@@ -1,12 +1,7 @@
 package image
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"net"
-	"net/http"
 	"time"
 
 	regauthn "github.com/google/go-containerregistry/pkg/authn"
@@ -58,17 +53,12 @@ func (i Registry) Image(ref regname.Reference) (regv1.Image, error) {
 }
 
 func (i Registry) WriteImage(ref regname.Reference, img regv1.Image) error {
-	httpTran, err := i.newHTTPTransport()
+	opts, err := i.imageOpts()
 	if err != nil {
 		return err
 	}
 
-	auth, err := i.registryKeychain().Resolve(ref.Context().Registry)
-	if err != nil {
-		return fmt.Errorf("Getting auth details: %s", err)
-	}
-
-	err = i.retry(func() error { return regremote.Write(ref, img, auth, httpTran) })
+	err = i.retry(func() error { return regremote.Write(ref, img, opts...) })
 	if err != nil {
 		return fmt.Errorf("Writing image: %s", err)
 	}
@@ -86,17 +76,12 @@ func (i Registry) Index(ref regname.Reference) (regv1.ImageIndex, error) {
 }
 
 func (i Registry) WriteIndex(ref regname.Reference, idx regv1.ImageIndex) error {
-	httpTran, err := i.newHTTPTransport()
+	opts, err := i.imageOpts()
 	if err != nil {
 		return err
 	}
 
-	auth, err := i.registryKeychain().Resolve(ref.Context().Registry)
-	if err != nil {
-		return fmt.Errorf("Getting auth details: %s", err)
-	}
-
-	err = i.retry(func() error { return regremote.WriteIndex(ref, idx, auth, httpTran) })
+	err = i.retry(func() error { return regremote.WriteIndex(ref, idx, opts...) })
 	if err != nil {
 		return fmt.Errorf("Writing image index: %s", err)
 	}
@@ -105,71 +90,30 @@ func (i Registry) WriteIndex(ref regname.Reference, idx regv1.ImageIndex) error 
 }
 
 func (i Registry) ListTags(repo regname.Repository) ([]string, error) {
-	httpTran, err := i.newHTTPTransport()
+	opts, err := i.imageOpts()
 	if err != nil {
 		return nil, err
 	}
 
-	auth, err := i.registryKeychain().Resolve(repo.Registry)
-	if err != nil {
-		return nil, fmt.Errorf("Getting auth details: %s", err)
-	}
-
-	return regremote.List(repo, auth, httpTran)
+	return regremote.List(repo, opts...)
 }
 
-func (i Registry) imageOpts() ([]regremote.ImageOption, error) {
-	httpTran, err := i.newHTTPTransport()
-	if err != nil {
-		return nil, err
-	}
+func (i Registry) imageOpts() ([]regremote.Option, error) {
+	var auth regremote.Option
 
-	return []regremote.ImageOption{
-		regremote.WithTransport(httpTran),
-		regremote.WithAuthFromKeychain(i.registryKeychain()),
-	}, nil
-}
+        switch {
+        case len(i.opts.Username) > 0:
+                auth = regremote.WithAuth(&regauthn.Basic{Username: i.opts.Username, Password: i.opts.Password})
+        case len(i.opts.Token) > 0:
+                auth = regremote.WithAuth(&regauthn.Bearer{Token: i.opts.Token})
+        case i.opts.Anon:
+                auth = regremote.WithAuth(regauthn.Anonymous)
+        default:
+		auth = regremote.WithAuthFromKeychain(regauthn.DefaultKeychain)
+        }
 
-func (i Registry) registryKeychain() regauthn.Keychain {
-	return customRegistryKeychain{i.opts}
-}
-
-func (i Registry) newHTTPTransport() (*http.Transport, error) {
-	pool, err := x509.SystemCertPool()
-	if err != nil {
-		pool = x509.NewCertPool()
-	}
-
-	if len(i.opts.CACertPaths) > 0 {
-		for _, path := range i.opts.CACertPaths {
-			if certs, err := ioutil.ReadFile(path); err != nil {
-				return nil, fmt.Errorf("Reading CA certificates from '%s': %s", path, err)
-			} else if ok := pool.AppendCertsFromPEM(certs); !ok {
-				return nil, fmt.Errorf("Adding CA certificates from '%s': failed", path)
-			}
-		}
-	}
-
-	// Copied from https://github.com/golang/go/blob/release-branch.go1.12/src/net/http/transport.go#L42-L53
-	// We want to use the DefaultTransport but change its TLSClientConfig. There
-	// isn't a clean way to do this yet: https://github.com/golang/go/issues/26013
-	return &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ResponseHeaderTimeout: 10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		// Use the cert pool with k8s cert bundle appended.
-		TLSClientConfig: &tls.Config{
-			RootCAs:            pool,
-			InsecureSkipVerify: (i.opts.VerifyCerts == false),
-		},
+	return []regremote.Option{
+		auth,
 	}, nil
 }
 
@@ -193,21 +137,4 @@ func (i Registry) retry(doFunc func() error) error {
 		time.Sleep(1 * time.Second)
 	}
 	return fmt.Errorf("Retried 5 times: %s", lastErr)
-}
-
-type customRegistryKeychain struct {
-	opts RegistryOpts
-}
-
-func (k customRegistryKeychain) Resolve(reg regname.Registry) (regauthn.Authenticator, error) {
-	switch {
-	case len(k.opts.Username) > 0:
-		return &regauthn.Basic{Username: k.opts.Username, Password: k.opts.Password}, nil
-	case len(k.opts.Token) > 0:
-		return &regauthn.Bearer{Token: k.opts.Token}, nil
-	case k.opts.Anon:
-		return regauthn.Anonymous, nil
-	default:
-		return regauthn.DefaultKeychain.Resolve(reg)
-	}
 }
